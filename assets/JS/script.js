16ba5f30dbc73811300ce1c606b59a07de8f812a
Joao Lucas <joaolucasgomesrocha@gmail.com>
2022-11-09 22:11:29 -0300

    Desenvolvimento tela login e footer

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('inputLogin');
+
+button.addEventListener('click', login())
+
+var usuario = document.getElementById('user')
+var senha = document.getElementById('password')
+
+function login() {
+  console.log('Deu certo')
+}
