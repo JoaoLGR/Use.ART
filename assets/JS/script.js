497605b032030569a138f1a17fe9c0c9eec66eb7
Joao Lucas <joaolucasgomesrocha@gmail.com>
2022-11-16 22:12:56 -0300

    Subindo site pro gitPages

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,3 +8,19 @@ var senha = document.getElementById('password')
 function login() {
   console.log('Deu certo')
 }
+/* Contador do slider */
+let count = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(function () {
+  nextImage()
+}, 2000)
+
+function nextImage() {
+  count++
+  if (count > 4) {
+    count = 1
+  }
+  document.getElementById('radio' + count).checked = true
+}
